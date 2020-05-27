77ad5c93c00b152db74f22f811173422e0352a70
Sourav <developer.srv1@gmail.com>
2020-05-27 22:47:15 +0530

    regex match for middleware mapping

--- application/libraries/twilight/middleware/Route.php
+++ application/libraries/twilight/middleware/Route.php
@@ -3,7 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Route {
 	protected $CI;
+
 	private bool $routeMatch = FALSE;
+
+    private static array $placeholderReplacements = [
+        '/\(:string\)/'  => '[a-zA-Z]+',
+        '/\(:num\)/'  => '[0-9]+',
+    ];
 
 	public function __construct()
 	{
@@ -30,6 +36,22 @@ class Route {
 	{
 		$self = new self();
 		if($self->CI->uri->segment($n) === $segment) {
+			$self->routeMatch = TRUE;
+		}
+
+		return $self;
+	}
+
+	/**
+	 * find regex segment match
+	 */
+	public static function match(string $pattern) : self
+	{
+		$pattern = str_replace('/', '\/', $pattern);
+		$pattern = preg_replace(array_keys(self::$placeholderReplacements), array_values(self::$placeholderReplacements), $pattern,1);
+
+		$self = new self();
+		if(preg_match('/'. $pattern .'/', $self->CI->uri->uri_string())) {
 			$self->routeMatch = TRUE;
 		}
 
--- application/middlewares/RouteMiddleware.php
+++ application/middlewares/RouteMiddleware.php
@@ -3,10 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-Route::is('')->apply('test.middleware');
+// Route::is('')->apply('test.middleware');
+Route::match('welcome/(:string)/(:num)')->apply('my.middleware');
 
-Route::is('welcome')->apply('test.middleware');
+// Route::is('welcome')->apply('test.middleware');
 
-Route::is('welcome/index')->apply(['test.middleware', 'my.middleware']);
+// Route::is('welcome/index')->apply(['test.middleware', 'my.middleware']);
 
-Route::segment(1, 'welcome')->apply(['test.middleware']);
+// Route::segment(1, 'welcome')->apply(['test.middleware']);
